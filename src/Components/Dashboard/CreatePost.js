fa23d942858bb40d2163229ffacef8e24ad00804
WarHammer Grantham <warhammergrantham@WarHammers-MacBook-Pro.local>
2017-05-20 17:23:12 -0700

    3rd Commit

--- src/Components/Dashboard/CreatePost.js
+++ src/Components/Dashboard/CreatePost.js
@@ -1,4 +1,13 @@
 import React, {Component} from 'react';
 
-class CreatePost extends Component 
+class CreatePost extends Component {
+    render(){
+        return (
+            <div>
+                <h1> a create post thing</h1>
+                <p>this is not a drill</p>
+            </div>
+        )
+    }
+}
 
